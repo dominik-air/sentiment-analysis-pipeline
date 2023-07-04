de6de2104a47383043e4eb3bbc785cce452ca443
dominik-air <domzurek@student.agh.edu.pl>
2023-07-04 17:25:56 +0200

    formatted with black

--- dags/write_postgres.py
+++ dags/write_postgres.py
@@ -9,8 +9,9 @@ from airflow.utils.dates import days_ago
 def insert_data():
     r = requests.get("http://34.90.170.212?n=10")
 
-    if r.status_code != 200: raise ValueError("API issue.")
-   
+    if r.status_code != 200:
+        raise ValueError("API issue.")
+
     df = pd.DataFrame(r.json())
 
     db_hook = PostgresHook(postgres_conn_id="postgres_sbx")
@@ -34,10 +35,10 @@ def insert_data():
 
         cursor.execute("SELECT MAX(id) FROM Tweets;")
         max_id = cursor.fetchone()[0]
-        
+
         if max_id is None:
             max_id = 0
-        
+
         new_id = max_id + 1
 
         insert_tweet_query = f"""
@@ -61,6 +62,7 @@ def insert_data():
 =======
 >>>>>>> now it should work
 
+
 with DAG(
     "create_and_populate_table_dag",
     default_args={
@@ -70,7 +72,7 @@ with DAG(
     schedule_interval="*/15 * * * *",
     start_date=days_ago(1),
     tags=["example"],
-    catchup=False
+    catchup=False,
 ) as dag:
     create_and_populate_table_task = PythonOperator(
         task_id="insert_data", python_callable=insert_data
